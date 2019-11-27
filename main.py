fff5fb320f17df2e16d4a83e4b7aecc2f07eaa11
Moacyr Pinheiro <moacyr.moacyr11@gmail.com>
2019-11-27 18:41:09 -0400

    Adicionados métodos para cadastrar pessoa, orgao, empresa, licitação e contrato

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from neo4j import GraphDatabase
 
 #print(cadastro_file.head())
 
-class HelloWorldExample(object):
+class new4jDriver(object):
 
     def __init__(self, uri, user, password):
         self._driver = GraphDatabase.driver(uri, auth=(user, password))
@@ -26,6 +26,37 @@ class HelloWorldExample(object):
                         "RETURN a.message + ', from node ' + id(a)", message=message)
         return result.single()[0]
 
-driverTeste = HelloWorldExample('bolt://localhost:7687',"neo4j", "123456")
-driverTeste.print_greeting('sad')
+    #Cadastrar pessoa
+    def add_pessoa(self, nome, cpf):
+        with self._driver.session() as session:
+            session.run("CREATE (a:Pessoa {nome: $nome, cpf: $cpf})", nome=nome, cpf=cpf)
+    
+    #Cadastrar orgão
+    def add_orgao(self, nome, codigo):
+        with self._driver.session() as session:
+            session.run("CREATE (a:Orgao {nome: $nome, codigo: $codigo})", nome=nome, codigo=codigo)
+            ()
+
+    #Cadastrar pessoa
+    def add_licitacao(self, numero, objeto, situacao, valor, data):
+        with self._driver.session() as session:
+            session.run("CREATE (:Licitacao {numero: $numero, objeto: $objeto, situacao:$situacao, valor:$valor, data:$data})", 
+            numero=numero, objeto=objeto, situacao=situacao, valor=valor, data=data)
+
+    #Cadastrar contrato
+    def add_contrato(self, numero, objeto, valor, data):
+        with self._driver.session() as session:
+            session.run("CREATE (:Licitacao {numero: $numero, objeto: $objeto, valor:$valor, data:$data})", 
+            numero=numero, objeto=objeto, valor=valor, data=data)
+
+    #Cadastrar empresa
+    def add_empresa(self, nome_social, nome_fantasia, cnpj):
+        with self._driver.session() as session:
+            session.run("CREATE (:Licitacao {nome_social: $nome_social, nome_fantasia:$nome_fantasia, cnpj:$cnpj})", 
+            nome_social=nome_social, nome_fantasia=nome_fantasia, cnpj=cnpj)
+
+
+driver = new4jDriver('bolt://localhost:7687',"neo4j", "123456")
+#driverTeste.print_greeting('sad')
+driver.add_licitacao('numero', 'objeto', 'situacao', 'valor', 'data')
 
