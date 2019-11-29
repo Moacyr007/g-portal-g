062b8e62a1532bf7fbd7ee0e57379e20531fb8c1
unknown <moacyr.moacyr11@gmail.com>
2019-11-28 17:13:41 -1200

    tentando corrigir erro ao inserir dados

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from neo4j import GraphDatabase
 import pandas as pd 
-from neo4j import GraphDatabase
+import csv
+
 
 class new4jDriver(object):
 
@@ -27,12 +30,11 @@ class new4jDriver(object):
         with self._driver.session() as session:
             session.run("CREATE (a:Pessoa {nome: $nome, cpf: $cpf})", nome=nome, cpf=cpf)
     
-    #Cadastrar orgão
+    #Cadastrar orgao
     def add_orgao(self, nome, codigo):
         with self._driver.session() as session:
             session.run("CREATE (a:Orgao {nome: $nome, codigo: $codigo})", nome=nome, codigo=codigo)
-            ()
-
+            
     #Cadastrar pessoa
     def add_licitacao(self, numero, objeto, situacao, valor, data):
         with self._driver.session() as session:
@@ -42,61 +44,89 @@ class new4jDriver(object):
     #Cadastrar contrato
     def add_contrato(self, numero, objeto, valor, data):
         with self._driver.session() as session:
-            session.run("CREATE (:Licitacao {numero: $numero, objeto: $objeto, valor:$valor, data:$data})", 
+            session.run("CREATE (:Contrato {numero: $numero, objeto: $objeto, valor:$valor, data:$data})", 
             numero=numero, objeto=objeto, valor=valor, data=data)
 
     #Cadastrar empresa
     def add_empresa(self, nome_social, nome_fantasia, cnpj):
         with self._driver.session() as session:
-            session.run("CREATE (:Licitacao {nome_social: $nome_social, nome_fantasia:$nome_fantasia, cnpj:$cnpj})", 
+            session.run("CREATE (:Empresa {nome_social: $nome_social, nome_fantasia:$nome_fantasia, cnpj:$cnpj})", 
             nome_social=nome_social, nome_fantasia=nome_fantasia, cnpj=cnpj)
 
 
 driver = new4jDriver('bolt://localhost:7687',"neo4j", "123456")
+
 '''
-cnpj_file = pd.read_csv('201908_CNPJ.csv', sep=';', encoding='cp860')
+#funcionando
+
+cnpj_file = pd.read_csv('201908_CNPJ.csv', sep=';', encoding='latin1')
+for index, row in cnpj_file.head().iterrows():
+    driver.add_empresa(row['RAZAOSOCIAL'], row['NOMEFANTASIA'],row['CNPJ'])
+
+cadastro_file = pd.read_csv('201908_Cadastro.csv', sep=';', encoding='latin1')
+for index, row in cadastro_file.head().iterrows():
+    driver.add_pessoa(row['NOME'], row['CPF'])
+'''
+'''
+licitacao_file = pd.read_csv('201908_Licitacao.csv', sep=';', encoding='latin1')
+print('AQUI _________', licitacao_file.columns)
+compras_file = pd.read_csv('201908_Compras.csv', sep=';', encoding='latin1')
+print('AQUI _________', compras_file.columns)
+'''
+
+licitacao_file = pd.read_csv('201908_Licitacao.csv', sep=';', encoding='latin1')
+licitacao_list = licitacao_file.values.tolist() 
+
+def dim(a):
+    if not type(a) == list:
+        return []
+    return [len(a)] + dim(a[0])
+
+print(dim(licitacao_list))
+#TODO tentar converter df pra lista ver se consigo inserir
+
+'''   
+licitacao_file = pd.read_csv('201908_Licitacao.csv', sep=';', encoding='latin1')
+
+for index, row in licitacao_file.head().iterrows():
+    driver.add_licitacao(row['Número Licitação'], row['Objeto'], row['Situação Licitação'], row['Valor Licitação'], row['Data Resultado Compra'])
+for index, row in licitacao_file.iterrows():
+    driver.add_orgao(row['Nome Órgão'], row['Código Órgão'])
+
+
+compras_file = pd.read_csv('201908_Compras.csv', sep=';', encoding='latin1')
+for index, row in licitacao_file.head().iterrows():
+    driver.add_contrato(row['Número do Contrato'], row['Objeto'], row['Valor Final Compra'], row['Data Início Vigência'])
+
+
+'''
+
+
+'''
+cnpj_file = pd.read_csv('201908_CNPJ.csv', sep=';', encoding='1252')
 for index, row in cnpj_file.iterrows():
     driver.add_empresa(row['RAZAOSOCIAL'], row['NOMEFANTASIA'],row['CNPJ'])
 
 
-cadastro_file = pd.read_csv('201908_Cadastro.csv', sep=';', encoding='cp860')
+cadastro_file = pd.read_csv('201908_Cadastro.csv', sep=';', encoding='1252')
 for index, row in cadastro_file.iterrows():
     driver.add_pessoa(row['NOME'], row['CPF'])
 
-licitacao_file = pd.read_csv('201908_Licitaá∆o.csv', sep=';', encoding='cp860')
+licitacao_file = pd.read_csv('201908_Licitaá∆o.csv', sep=';', encoding='1252')
 for index, row in licitacao_file.iterrows():
     driver.add_licitacao(row['Número Licitação'], row['Objeto'], row['Situação Licitação'], row['Valor Licitação'], row['Data Resultado Compra'])
 
 for index, row in licitacao_file.iterrows():
     driver.add_orgao(row['Nome Órgão'], row['Código Órgão'])
 
-compras_file = pd.read_csv('201908_Compras.csv', sep=';', encoding='cp860')
+compras_file = pd.read_csv('201908_Compras.csv', sep=';', encoding='1252')
 for index, row in licitacao_file.iterrows():
     driver.add_contrato(row['Número do Contrato'], row['Objeto'], row['Valor Final Compra'], row['Data Início Vigência'])
-
 '''
-
-cnpj_file = pd.read_csv('201908_CNPJ.csv', sep=';', encoding='cp860')
-for index, row in cnpj_file.head().iterrows():
-    driver.add_empresa(row['RAZAOSOCIAL'], row['NOMEFANTASIA'],row['CNPJ'])
-
-
-cadastro_file = pd.read_csv('201908_Cadastro.csv', sep=';', encoding='cp860')
-for index, row in cadastro_file.head().iterrows():
-    driver.add_pessoa(row['NOME'], row['CPF'])
-
-licitacao_file = pd.read_csv('201908_Licitaá∆o.csv', sep=';', encoding='cp860')
-for index, row in licitacao_file.head().iterrows():
-    driver.add_licitacao(row['Número Licitação'], row['Objeto'], row['Situação Licitação'], row['Valor Licitação'], row['Data Resultado Compra'])
-
-for index, row in licitacao_file.iterrows():
-    driver.add_orgao(row['Nome Órgão'], row['Código Órgão'])
-
-compras_file = pd.read_csv('201908_Compras.csv', sep=';', encoding='cp860')
-for index, row in licitacao_file.head().iterrows():
-    driver.add_contrato(row['Número do Contrato'], row['Objeto'], row['Valor Final Compra'], row['Data Início Vigência'])
-
-
-
-
+'''
+with open('201908_CNPJ.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=';', encoding='1252')
+    for row in readCSV:
+        print(row) 
+'''
 
